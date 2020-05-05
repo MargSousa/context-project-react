028e3e06c9c535bffac9104e3f3483677cccd746
Margarida Sousa <margaridasousa@MBPdeMargarida2.lan>
2020-05-05 12:59:22 +0100

    setup done

---
+++ src/components/AddItemInput.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import List from './List';
+
+const AddItemInput = () => {
+  return (
+    <div className="input">
+      <List />
+    </div>
+  );
+}
+ 
+export default AddItemInput;
